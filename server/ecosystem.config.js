98ab109aab7680325bb22a14963084a7587a56ec
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-01-28 22:06:33 -0800

    Setup config for Azure (#97)

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,20 +1,21 @@
+require("dotenv").config();
+
 module.exports = {
   apps: [
     {
-      name: "Portfolio_Instruments_Server",
+      name: "api.portfolioinstruments",
       script: "./dist/app.js",
       env_production: {
         NODE_ENV: "production",
-        DB_USERNAME: "",
-        DB_PASSWORD: "",
-        DB_TITLE: "",
-        DB_HOST: "",
-        GMAIL_USER: "",
-        GMAIL_PASS: "",
-        CLIENT_BASE_URL: "",
-        SERVER_BASE_URL: "",
-        JWT_SECRET: "",
-        EMAIL_SECRET: "",
+        DB_USERNAME: process.env.DB_USERNAME,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_TITLE: process.env.DB_TITLE,
+        DB_HOST: process.env.DB_HOST,
+        GMAIL_USER: process.env.GMAIL_USER,
+        GMAIL_PASS: process.env.GMAIL_PASS,
+        SERVER_BASE_URL: process.env.SERVER_BASE_URL,
+        JWT_SECRET: process.env.JWT_SECRET,
+        EMAIL_SECRET: process.env.EMAIL_SECRET,
       },
       env_development: {
         NODE_ENV: "development",
