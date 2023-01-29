09d301ec5e41f9403af9fc0731a71f7af8fea100
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-01-29 00:05:43 -0800

    Update client to point to new backend setup (#98)

--- client/src/redux/api/endpoints/constants.ts
+++ client/src/redux/api/endpoints/constants.ts
@@ -1,30 +1,32 @@
+const baseUrl: string = 'https://api.portfolioinstruments.com';
+
 // API Endpoints
 export const USER_ENDPOINT = {
-  LOGIN: "/api/login",
-  REGISTER: "/api/register",
-  EMAIL_CONFIRMATION: "/api/confirmation",
-  CHANGE_NOTIFICATIONS: "/api/changeNotifications",
-  CHANGE_PASSWORD: "/api/changePassword",
-  RESET_PASSWORD: "/api/resetPassword",
-  DELETE_USER: "/api/deleteUser",
+  LOGIN: `${baseUrl}/api/login`,
+  REGISTER: `${baseUrl}/api/register`,
+  EMAIL_CONFIRMATION: `${baseUrl}/api/confirmation`,
+  CHANGE_NOTIFICATIONS: `${baseUrl}/api/changeNotifications`,
+  CHANGE_PASSWORD: `${baseUrl}/api/changePassword`,
+  RESET_PASSWORD: `${baseUrl}/api/resetPassword`,
+  DELETE_USER: `${baseUrl}/api/deleteUser`,
 };
 
 export const SNAPSHOT_ENDPOINT = {
-  GET_LATEST: "/api/snapshots/latest",
-  GET_RANGE: (years: number) => `/api/snapshots/range/${years}`,
-  GET_ALL: "/api/snapshots/all",
-  POST_SNAPSHOT: "/api/snapshots",
-  DELETE_SNAPSHOT: "/api/snapshots",
+  GET_LATEST: `${baseUrl}/api/snapshots/latest`,
+  GET_RANGE: (years: number) => `${baseUrl}/api/snapshots/range/${years}`,
+  GET_ALL: `${baseUrl}/api/snapshots/all`,
+  POST_SNAPSHOT: `${baseUrl}/api/snapshots`,
+  DELETE_SNAPSHOT: `${baseUrl}api/snapshots`,
 };
 
 export const BENCHMARK_ENDPOINT = {
-  GET_BENCHMARK: "/api/benchmarks",
-  SET_BENCHMARK: "/api/benchmarks",
-  GET_CUSTOM_BENCHMARK: "/api/benchmarks/custom",
-  SET_CUSTOM_BENCHMARK: "/api/benchmarks/custom",
-  REMOVE_FROM_CUSTOM: "/api/benchmarks/custom",
+  GET_BENCHMARK: `${baseUrl}/api/benchmarks`,
+  SET_BENCHMARK: `${baseUrl}/api/benchmarks`,
+  GET_CUSTOM_BENCHMARK: `${baseUrl}/api/benchmarks/custom`,
+  SET_CUSTOM_BENCHMARK: `${baseUrl}/api/benchmarks/custom`,
+  REMOVE_FROM_CUSTOM: `${baseUrl}/api/benchmarks/custom`,
 };
 
 export const LOG_ENDPOINT = {
-  GET_RECORDS_JSON: (years: string) => `/api/logs/records/json/${years}`,
+  GET_RECORDS_JSON: (years: string) => `${baseUrl}/api/logs/records/json/${years}`,
 };
